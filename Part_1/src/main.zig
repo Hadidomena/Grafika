cbf42478ef2d99af3c68e766754bba46722f7da7
Hadidomena <szepiet33@gmail.com>
2026-04-15 01:16:56 +0200

    remove placeholders from main

--- Part_1/src/main.zig
+++ Part_1/src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const Io = std.Io;
 
 const Part_1 = @import("root.zig");
 
@@ -289,45 +288,8 @@ pub fn main(init: std.process.Init) !void {
     edges_list.deinit(arena);
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa);
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
 test "raylib integration - gen image (CPU)" {
     const img = c.GenImageColor(2, 2, c.RAYWHITE);
     try std.testing.expect(c.IsImageValid(img));
     c.UnloadImage(img);
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
 }
